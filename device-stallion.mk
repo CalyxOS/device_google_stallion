59800a501e7cf7d007a05afbf51182529d015614
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    stallion: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-stallion.mk
+++ device-stallion.mk
@@ -25,6 +25,10 @@ include device/google/zumapro/common.mk
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/config/default_volume_tables.xml
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     AmbientCueOverlay \
