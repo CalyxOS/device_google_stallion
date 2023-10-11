7888b121e72ca996a6ad9bef091eaf5cc8a06110
Roshan Pius <rpius@google.com>
2023-10-11 11:38:55 -0700

    tegu: Replace NFC app with NFC apex
    Bug: 303286040
    Test: Compiles
    Change-Id: I8b2916c79863d7a3304ff80537a80248deefc1a3

--- device-tegu.mk
+++ device-tegu.mk
@@ -75,7 +75,7 @@ PRODUCT_COPY_FILES += \
 	device/google/tegu/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	NfcNci \
+	com.android.nfcservices \
 	Tag \
 	android.hardware.nfc-service.st
 
