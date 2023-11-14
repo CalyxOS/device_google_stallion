ca0393b1712ee588b76ef70a3ed543c74064ba8b
JohnCH Tsai <tsaijohn@google.com>
2023-11-14 09:55:16 +0000

    tegu: add tegu for camera hardware
    Bug: 310549706
    Test: build and update com.google.pixel.camera.hal
    Change-Id: I76cbb09f6aa39fe62f395eff04022adf38ff8d85

--- device-tegu.mk
+++ device-tegu.mk
@@ -43,7 +43,7 @@ include device/google/tegu/fingerprint/ultrasonic_udfps.mk
 # go/lyric-soong-variables
 # # TODO(298309659): Needs to check with owner later
 $(warning camera_hardware set to zuma on zumapro target)
-$(call soong_config_set,lyric,camera_hardware,ripcurrent)
+$(call soong_config_set,lyric,camera_hardware,tegu)
 $(warning tuning_product set to zuma on zumapro target)
 $(call soong_config_set,lyric,tuning_product,ripcurrent)
 $(warning target_device set to zuma on zumapro target)
