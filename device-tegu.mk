1c2d9f35de781379d120513d3c23e774e2f26654
Blade Chiu <bladechiu@google.com>
2024-03-06 17:37:58 +0800

    Set tegu as tuning product for tegu's device-tegu.mk file
    Bug: 327008589
    Test: Tested on tegu deice
    Change-Id: Ic0708df90e4254cc512a783ed8854114ca074e3d

--- device-tegu.mk
+++ device-tegu.mk
@@ -41,9 +41,9 @@ include device/google/gs-common/touch/syna/syna20.mk
 $(warning camera_hardware set to zuma on zumapro target)
 $(call soong_config_set,lyric,camera_hardware,tegu)
 $(warning tuning_product set to zuma on zumapro target)
-$(call soong_config_set,lyric,tuning_product,ripcurrent)
+$(call soong_config_set,lyric,tuning_product,tegu)
 $(warning target_device set to zuma on zumapro target)
-$(call soong_config_set,google3a_config,target_device,ripcurrent)
+$(call soong_config_set,google3a_config,target_device,tegu)
 
 # display
 DEVICE_PACKAGE_OVERLAYS += device/google/tegu/tegu/overlay
