953ee73324890c3a3a4f5200e7996fe32333f1dc
Frank Yu <shengfanyu@google.com>
2024-05-31 07:13:12 +0000

    Enable radio ext aidl service for dynamic ssc.
    Bug: 343576955
    Test: Manual. RadioExt aidl service start successfully.
    Change-Id: Ia2b7ff1c14fae8b7aedf40a594b346359072dc6a

--- device-tegu.mk
+++ device-tegu.mk
@@ -59,6 +59,7 @@ include hardware/google/pixel/vibrator/cs40l26/device.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
 include device/google/gs-common/touch/gti/predump_gti.mk
 include device/google/gs-common/touch/syna/predump_syna20.mk
+include device/google/gs-common/modem/radio_ext/radio_ext.mk
 
 # go/lyric-soong-variables
 $(call soong_config_set,lyric,camera_hardware,tegu)
