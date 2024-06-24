3844339e4194862461e4a4b9d01c8b9125235e85
Jason Chang <jasonsfchang@google.com>
2024-06-24 07:05:50 +0000

    Enabled One-handed mode in new project.
    Set PRODUCT_PRODUCT_PROPERTIES ro.support_one_handed_mode=true
    
    Flag: EXEMPT bugfix
    
    Bug: 346930480
    
    Test: build ABTD ROM to verify One-handed mode existence
    Change-Id: Ib8076d061e8820a8ef5aee12475ddba80b7b2357

--- device-tegu.mk
+++ device-tegu.mk
@@ -219,6 +219,10 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.server.automatic_turn_on=true
 
+# Set support One-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 # Keymaster HAL
 #LOCAL_KEYMASTER_PRODUCT_PACKAGE ?= android.hardware.keymaster@4.1-service
 
