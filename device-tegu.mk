e7e337df286a757d68e76d1fc9a478bb06e8f35b
kellyhung <kellyhung@google.com>
2024-08-29 15:05:15 +0800

    Add a soong config USE_RADIOEXTERNAL_HAL_AIDL in device/google/tegu/device-tegu.mk
    Bug: 360757079
    Test: lunch r11-trunk_staging-userdebug; m grilservice
    Flag: EXEMPT bugfix
    
    Change-Id: Ib8139b30f976c6b5f41686e9263b9da7ff87db07

--- device-tegu.mk
+++ device-tegu.mk
@@ -73,6 +73,7 @@ PRODUCT_COPY_FILES += \
 # USE_OEMSERVICE_HAL_AIDL := true
 # Enable AIDL based radioExternal HAL
 USE_RADIOEXTERNAL_HAL_AIDL := true
+$(call soong_config_set,grilservice,USE_RADIOEXTERNAL_HAL_AIDL,true)
 
 # NFC
 PRODUCT_COPY_FILES += \
