ee1826ecc99bbcef6f442235f891bd375f14dbfe
Neo Yu <neoyu@google.com>
2024-09-05 01:07:18 +0800

    Enable radioext 2.0 and include gril_aidl.mk for tegu
    Bug: 283064367
    Test: verify with test ROMs
    Flag: EXEMPT HAL interface change
    Change-Id: I6cd2e3c5b7aae8c51fc07c7e86a992260073c878

--- device-tegu.mk
+++ device-tegu.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+USES_RADIOEXT_V1_7 = false
+USES_RADIOEXT_V2_0 = true
+
 ifdef RELEASE_GOOGLE_TEGU_RADIO_DIR
 RELEASE_GOOGLE_PRODUCT_RADIO_DIR := $(RELEASE_GOOGLE_TEGU_RADIO_DIR)
 endif
@@ -52,8 +55,9 @@ include hardware/google/pixel/vibrator/cs40l26/device.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
 include device/google/gs-common/touch/gti/predump_gti.mk
 include device/google/gs-common/touch/syna/predump_syna20.mk
-include device/google/gs-common/modem/radio_ext/radio_ext.mk
+include device/google/gs-common/gril/aidl/2.0/gril_aidl.mk
 include device/google/gs-common/esim/esim.mk
+
 # go/lyric-soong-variables
 $(call soong_config_set,lyric,camera_hardware,tegu)
 $(call soong_config_set,lyric,tuning_product,tegu)
