48fb253d517d299a1d144fe88a1ad10f0ed4747a
Nicole Lee <nicoleytlee@google.com>
2024-09-10 16:38:00 +0000

    tegu: enable IOemService AIDL
    Bug: 356025857
    Test: Start modem logging
    Flag: EXEMPT HAL interface change
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:45a9a05486a31ca103ad8cb9141cab1b83aefd61)
    Merged-In: I9831129bdb5eaceda4579845d30c2e1f53d9ee68
    Change-Id: I9831129bdb5eaceda4579845d30c2e1f53d9ee68

--- device-tegu.mk
+++ device-tegu.mk
@@ -77,7 +77,7 @@ PRODUCT_COPY_FILES += \
         device/google/tegu/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.tegu.rc
 
 # Enable AIDL based oemservice HAL
-# USE_OEMSERVICE_HAL_AIDL := true
+USE_OEMSERVICE_HAL_AIDL := true
 # Enable AIDL based radioExternal HAL
 USE_RADIOEXTERNAL_HAL_AIDL := true
 
