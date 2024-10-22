3123f21cb26ee7754ae211dbfe30ca211379df48
kuanyuhuang <kuanyuhuang@google.com>
2024-10-22 03:04:16 +0000

    [BT] Add SAR tables
    Add Bluetooth SAR table for differnet SKUs
    
    Bug: 369254971
    Test: build and check files at device
    Flag: EXEMPT sar table update
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0f04779317645d6b822cab03a76fd84949b3fcd2)
    Merged-In: I5e20434aa0eead6fb5173183b3ed4069a5c58155
    Change-Id: I5e20434aa0eead6fb5173183b3ed4069a5c58155

--- device-tegu.mk
+++ device-tegu.mk
@@ -139,7 +139,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Bluetooth Tx power caps
 PRODUCT_COPY_FILES += \
-    device/google/tegu/bluetooth/bluetooth_power_limits.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits.csv
+    device/google/tegu/bluetooth/bluetooth_power_limits_tegu.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits.csv \
+    device/google/tegu/bluetooth/bluetooth_power_limits_tegu_EU.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits_EU.csv \
+    device/google/tegu/bluetooth/bluetooth_power_limits_tegu_JP.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits_JP.csv \
+    device/google/tegu/bluetooth/bluetooth_power_limits_tegu_US_CA.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits_US.csv \
+    device/google/tegu/bluetooth/bluetooth_power_limits_tegu_US_CA.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits_CA.csv
 
 # POF
 PRODUCT_PRODUCT_PROPERTIES += \
