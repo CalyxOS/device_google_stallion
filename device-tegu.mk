1c2f8e7a16a78125b34f2c8f0a9a4bc3a25d5c55
Wilson Sung <wilsonsung@google.com>
2024-12-24 19:11:12 -0800

    tegu: Update SVN to 2
    Flag: build.RELEASE_SVN_TEGU
    Bug: 383638176
    Test: boot
    (cherry picked from commit d53b263bfbe0e432229503aa508a6087e8a7fb45)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d88eee4e8711a7194d7f9d63fd6697cfcfabdca4)
    Merged-In: Ibc84d73b0e1a496872b8d904b91655fc2cfb28ec
    Change-Id: Ibc84d73b0e1a496872b8d904b91655fc2cfb28ec

--- device-tegu.mk
+++ device-tegu.mk
@@ -434,3 +434,17 @@ PRODUCT_COPY_FILES += \
 #Component Override for Pixel Troubleshooting App
 PRODUCT_COPY_FILES += \
     device/google/tegu/tegu-component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/tegu-component-overrides.xml
+
+# Enable APF by default
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.powerhal.apf_disabled=false \
+    vendor.powerhal.apf_enabled=true
+
+# Increment the SVN for any official public releases
+ifdef RELEASE_SVN_TEGU
+TARGET_SVN ?= $(RELEASE_SVN_TEGU)
+else
+# Set this for older releases that don't use build flag
+TARGET_SVN ?= 2
+endif
+
