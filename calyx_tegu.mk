1643f5cbd1e77096ddfb0909ee32f1db5f07fee5
Michael Bestas <mkbestas@gmail.com>
2026-03-18 03:09:47 +0200

    Initial CalyxOS makefile
    Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

---
+++ calyx_tegu.mk
@@ -0,0 +1,15 @@
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from lineage device
+$(call inherit-product, device/google/stallion/lineage_stallion.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
